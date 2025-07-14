0434beb711a8e72846a4341cf630ebd4c3c8194e
Chehronai Fozil <chehronaifozil@gmail.com>
2025-07-13 21:07:23 -0400

    fixed config

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom";
-import "./index.css";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
+import './index.css';
+import App from './App';
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+    document.getElementById('root') as HTMLElement,
 );
 
 root.render(
-  <HashRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </HashRouter>
+    <HashRouter>
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    </HashRouter>,
 );
